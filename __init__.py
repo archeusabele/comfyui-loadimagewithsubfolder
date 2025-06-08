ce7eae2ff5f192147f78b7c36090902778433154
archeusabele <stockfootagedocumentary@gmail.com>
2025-06-08 20:38:50 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LoadImageWithSubfolder": "Load Image (With Subfolder)",
+    "LoadImageWithSubfolder": "Load Image (With Subfolder) Advanced",
 }
 
 
